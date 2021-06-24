d54de9f1a1b82fb7f9d37a7fd37ff4a1fd38be65
jpe <patriot.game@gmail.com>
2021-06-24 09:44:46 -0400

    fix border transparency

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#504945";
 static const char col_gray3[]       = "#bdae93";
 static const char col_gray4[]       = "#ebdbb2";
-static const char col_cyan[]        = "#458588";
+static const char col_cyan[]        = "#076678";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
